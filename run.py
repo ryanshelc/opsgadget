e52464b68cc8c3593db034f09cfa54025af6f116
Ryan Shelc <ryan.shelc@gmail.com>
2014-04-28 13:05:17 -0700

    initial commit

---
+++ run.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from werkzeug.contrib.fixers import ProxyFix
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+        return 'Hello World'
+
+app.wsgi_app = ProxyFix(app.wsgi_app)
+
+if __name__ == '__main__':
+        app.run(host='0.0.0.0',debug=True)
