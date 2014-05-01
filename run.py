a42c1c4cc744859f9d2aedda08005a12e51335cb
Ryan Shelc <ryan.shelc@gmail.com>
2014-04-30 19:48:53 -0700

    added in simple blog layout

--- run.py
+++ run.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from flask import render_template
 from werkzeug.contrib.fixers import ProxyFix
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-        return 'Hello World'
+	return render_template('index.html')
 
 app.wsgi_app = ProxyFix(app.wsgi_app)
 
